493a45641634d7a688d54e1e86a6ea1c1579eff3
Gina Enjelina <114559111+GinaEnjlina@users.noreply.github.com>
2026-06-01 22:30:02 +0700

    build: configure GitHub Pages deployment
    Update vite base path and add workflow to automate deployment to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Birthday-SuperMarioTheme/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
